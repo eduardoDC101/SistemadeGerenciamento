8e9297be3da7be40840198c061b39c350cb3103c
Eduardo Dourado <166930671+eduardoDC101@users.noreply.github.com>
2025-03-27 06:06:15 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import "./styles/index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
+  
   <React.StrictMode>
+  
     <App />
+  
   </React.StrictMode>
+
 );
